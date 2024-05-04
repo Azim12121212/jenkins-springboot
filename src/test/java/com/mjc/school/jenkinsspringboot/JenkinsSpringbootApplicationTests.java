31c4ef52be84042e76abe0e417b4c58e1e218926
Azimbek <omar.azimbek@gmail.com>
2024-05-04 23:56:35 +0600

    second commit

---
+++ src/test/java/com/mjc/school/jenkinsspringboot/JenkinsSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjc.school.jenkinsspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
